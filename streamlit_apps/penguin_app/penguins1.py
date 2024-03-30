23db4ace020991d5e6229ab664dbbceb5fae9cea
tehmeerali786 <freelanceali786@gmail.com>
2024-03-30 13:49:59 -0700

    Chapter 2

--- streamlit_apps/penguin_app/penguins1.py
+++ streamlit_apps/penguin_app/penguins1.py
@@ -2,20 +2,35 @@ import streamlit as st
 import pandas as pd 
 import altair as alt 
 import seaborn as sns 
+import time
 
 st.title("Palmer's Penguins")
 st.markdown('Use the streamlit app to make your own scatterplot about penguins!')
 
+penguin_file = st.file_uploader("Select your Local Penguins CSV (default provided)")
+
+@st.cache_data()
+def load_file(penguin_file):
+    time.sleep(3)
+    if penguin_file is not None:
+        df = pd.read_csv(penguin_file)
+    else: 
+        df = pd.read_csv("penguins.csv")
+    return (df)
+
+penguins_df = load_file(penguin_file)
 selected_x_var = st.selectbox('What do you want the x variable to be?',
                               ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
 selected_y_var = st.selectbox('What about the y?', 
                               ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
-penguin_file = st.file_uploader("Select your Local Penguins CSV (default provided)")
-if penguin_file is not None:
-    penguins_df = pd.read_cvs(penguin_file)
-else: 
-    st.stop()
+selected_gender = st.selectbox("What do you wan to filter for?", ["all penguins", "male penguins", "female penguins"])
 
+if selected_gender == "male penguins":
+    penguins_df = penguins_df[penguins_df["sex"] == "male"]
+elif selected_gender == "female penguins":
+    penguins_df = penguins_df[penguins_df["sex"] == "female"]
+else:
+    pass
 
 
 
